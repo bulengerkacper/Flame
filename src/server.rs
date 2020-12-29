76944c99ec90c385b7fe977f2a47d45e3fce9946
bulengerkacper <bulenger.kacper@gmail.com>
2020-12-29 15:16:41 +0100

    Server implementation changes

--- src/server.rs
+++ src/server.rs
@@ -1,13 +1,16 @@
 struct Server {
     hostname: String,
+    username: String,
     password: String,
 }
 
 impl Server {
-    pub fn new(hostname: String, password: String) -> Server {
+    pub fn new(hostname: String,username:String, password: String) -> Server {
         Server {
             hostname: hostname,
+            username: username,
             password: password,
         }
     }
+
 }
