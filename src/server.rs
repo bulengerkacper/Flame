ab23f2ac3089d8c52a1f20501782bb624851b0c7
bulengerkacper <bulenger.kacper@gmail.com>
2020-12-29 20:25:37 +0100

    added command execution method

--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,14 @@
+extern crate ssh2;
+use ssh2::Session;
+
 pub struct Server {
     pub hostname: String,
     pub username: String,
     pub password: String,
 }
 
-impl Server {}
+impl Server {
+    fn execute_command(self, session: &mut Session) {
+
+    }
+}
